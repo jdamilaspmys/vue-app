8e0b491d60afa5d21b79ccb987085ba7ea4efb57
jdamilaspmys <jdamilaspmys@gmail.com>
2020-02-22 12:13:40 +0800

    add cart and  btn, remove btn

--- main.js
+++ main.js
@@ -6,8 +6,31 @@ var app = new Vue({
         inStock: true,
         details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
         variants: [
-            { variantId: 2234, variantColor: "green"},
-            { variantId: 2235, variantColor: "blue"}
-        ]
-    }
+            { 
+                variantId: 2234, 
+                variantColor: "green",
+                variantImage: "/assets/images/vmSocks-green-onWhite.jpg"
+
+            },
+            { 
+                variantId: 2235, 
+                variantColor: "blue",
+                variantImage: "/assets/images/vmSocks-blue-onWhite.jpg"
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {            
+            this.cart += 1
+        },
+        remove(){
+            if(this.cart){
+                this.cart -= 1
+            }
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
+    },
 })
