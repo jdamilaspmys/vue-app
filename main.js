e9f62528f08c3dbd6c2b95116ce9fa045820dfb3
jdamilaspmys <jdamilaspmys@gmail.com>
2020-02-22 11:41:11 +0800

    add v-for as variant.variantColor

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ var app = new Vue({
     data: {
         product : "product",
         image: '/assets/images/vmSocks-green-onWhite.jpg',
-        inStock: true
+        inStock: true,
+        details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
+        variants: [
+            { variantId: 2234, variantColor: "green"},
+            { variantId: 2235, variantColor: "blue"}
+        ]
     }
 })
